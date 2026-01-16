4bf0397ddeaef1814e0ad419fac2370c44ac3584
hamzazirari <hamzazirari6002@gmail.com>
2026-01-16 15:29:23 +0100

    feat : add class diagramme and usecase

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -1,8 +1,11 @@
 <?php
 class CategoriesController extends BaseController {
     
-    public function listAction(): void {
-        
-    }
+  public function listAction(): void {
+    $manager = new CategoryManager($this->pdo);
+        $categories = $manager->getAllWithVehicules();
+    
+    $this->render('categories/list', ['categories' => $categories]);
+}
 }
 ?>
